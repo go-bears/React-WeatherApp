94c7b03f1ab39a34b9b34510e275b3795f2f8181
go-bears <melissa.fabros@gmail.com>
2016-07-06 20:08:11 -0700

    intial index.hmtl and jsx files

---
+++ public/app.jsx
@@ -0,0 +1,8 @@
+ReactDOM.render(
+    //first argument is what to render
+    <h1>Hello React!</h1>,
+    // second argument is where to render it, 
+    // here in this case the div named 'app'
+    document.getElementById('app')
+
+    );
